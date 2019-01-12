481480be732791eb9c7f7d802e6236109956b43e
Freddie Ridell <contact@freddieridell.com>
2019-01-12 21:00:41 +0000

    moved away from mono-repo, added meta info to package.json

--- packages/react-glot/src/GlotConsumer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Context from "./context";
-
-const GlotConsumer = ({ children }) => (
-	<Context.Consumer>{children}</Context.Consumer>
-);
-
-export default GlotConsumer;
--- packages/react-glot/src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Glot } from "./Glot"
-export { default as GlotConsumer } from "./GlotConsumer"
-export { default as GlotProvider } from "./GlotProvider"
